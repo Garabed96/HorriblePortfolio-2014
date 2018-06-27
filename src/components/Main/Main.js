d60ee3999591febbf5bd5f30edd2dc0549690d3e
Garabed96 <garo567@hotmail.com>
2018-06-27 03:27:05 -0400

    particle-js implementation complete for main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,21 +4,6 @@ import { Parallax, Background } from 'react-parallax';
 import Particles from 'react-particles-js';
 
 
-const particleOpt = {
-  particles: {
-    number: {
-      value: 60, 
-      density: {
-        enable: true, 
-        value_area: 800
-      }
-    },
-    color: {
-      value: "#ffffff", 
-    }, 
-  }
-}
-
 {/* <Particles param={particleOpt} /> */}
 
 class Main extends React.Component {
@@ -33,7 +18,72 @@ class Main extends React.Component {
       >
         <div style={{ height: '200px' }} />
         <div className="container">  
-        <Particles param={particleOpt} />
+        <Particles 
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+
+          params={{
+            number: {
+              value: 60, 
+              density: {
+                enable: true, 
+                value_area: 800
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1
+              },
+            }, 
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              polygon: {
+                nb_sides: 7
+              }
+            },
+            "interactivity": {
+              "detect_on": "canvas",
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "grab"
+                },
+                "onclick": {
+                  "enable": true,
+                  "mode": "push"
+                },
+                "resize": true
+              },
+              "modes": {
+                "grab": {
+                  "distance": 400,
+                  "line_linked": {
+                    "opacity": 1
+                  }
+                },
+                "repulse": {
+                  "distance": 200,
+                  "duration": 0.4
+                }
+              }
+            },
+            "retina_detect": true
+
+          }}
+        
+        
+        />
        
           <div className="container_title"> 
                 Hello, I'm Garo! 
